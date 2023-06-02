1d69956110f835aed52d1f3cd37b5f25777e5592
ThapLV <quangthap9x@gmail.com>
2023-06-02 19:35:26 +0700

    docker

--- client/src/configs/socketConfig.ts
+++ client/src/configs/socketConfig.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.REACT_APP_SERVER_ADDRESS || 'http://localhost:3000';
+const URL = process.env.REACT_APP_SERVER_ADDRESS || 'http://localhost:3003';
 
 export const socket = io(URL);
