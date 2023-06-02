1bfc93caa5db6619a454e75d7ffec6759c404727
ThapLV <quangthap9x@gmail.com>
2023-06-02 20:14:01 +0700

    edit config

--- client/src/configs/socketConfig.ts
+++ client/src/configs/socketConfig.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.REACT_APP_SERVER_ADDRESS || 'http://localhost:3003';
+const URL = process.env.REACT_APP_SERVER_ADDRESS || 'http://34.226.140.22:3003';
 
 export const socket = io(URL);
